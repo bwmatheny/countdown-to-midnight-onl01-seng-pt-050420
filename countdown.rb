eebaf7072742bb6f8e756f518a3afd5f6d50b5a6
Brian Matheny <bwmatheny@gmail.com>
2020-05-08 22:39:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,12 @@
 #write your code here
 
 def countdown(number)
-  loop do
+  while number > 0
 
     puts "#{number} SECOND(S)!"
     sleep 1
     number -= 1
-
-    break if number = 0
+     
   end
 
 
