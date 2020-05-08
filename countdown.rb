8b2abb0942858b2d864a4f105eae0974acbd7f31
Brian Matheny <bwmatheny@gmail.com>
2020-05-08 21:55:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 
 def countdown(number)
+loop
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep 1
   end
-
+  puts "HAPPY NEW YEAR!"
 end
-"HAPPY NEW YEAR!"
+end
